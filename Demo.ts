cea6f9cf9ff097908c981beaa3d8d20dc4ab4c7a
Desislav Ivanov <20824840+EvolveYourMind@users.noreply.github.com>
2021-12-15 13:04:14 +0100

    Add demo

--- Demo.ts
+++ Demo.ts
@@ -11,15 +11,22 @@ type one = Parse<`${SUCC} ${ZERO}`>
 type two = Parse<`${SUCC} (${SUCC} ${ZERO})`>
 
 type rawOne = Stringify<one> // ((λa.(λf.(λx.((a f) (f x))))) (λf.(λx.x)))
-type rawTwo = Stringify<two> // ((λa.(λf.(λx.((a f) (f x))))) ((λa.(λf.(λx.((a f) (f x))))) (λf.(λx.x))))
 
+type rawTwo = Stringify<two> // ( 
+                             //  (λa.(λf.(λx.((a f) (f x))))) 
+                             //   ((λa.(λf.(λx.((a f) (f x))))) (λf.(λx.x)))
+                             // )
+                             
 type evalZero = Stringify<Eval<zero>>  // (λf.(λx.x))
 type evalOne = Stringify<Eval<one>>  // (λf.(λx.(f x)))
 type evalTwo = Stringify<Eval<two>>  // (λf.(λx.(f (f x))))
 
 // 3 + 2 = 5
 type parsed = Parse<`${ADD} (${SUCC} (${SUCC} (${SUCC} ${ZERO}))) (${SUCC} (${SUCC} ${ZERO}))`>
-type rawParsed = Stringify<parsed> // (((λa.(λb.((b (λa.(λf.(λx.((a f) (f x)))))) a))) ((λa.(λf.(λx.((a f) (f x))))) ((λa.(λf.(λx.((a f) (f x))))) ((λa.(λf.(λx.((a f) (f x))))) (λf.(λx.x)))))) ((λa.(λf.(λx.((a f) (f x))))) ((λa.(λf.(λx.((a f) (f x))))) (λf.(λx.x)))))
-type ThreePlusTwo = Stringify<Eval<parsed>>
- // (λf.(λxx.(f (f (f (f (f xx)))))))
-  
+type rawParsed = Stringify<parsed> // (((λa.(λb.((b (λa.(λf.(λx.((a f) (f x)))))) a))) 
+                                   // ((λa.(λf.(λx.((a f) (f x))))) 
+                                   // ((λa.(λf.(λx.((a f) (f x)))))
+                                   // ((λa.(λf.(λx.((a f) (f x))))) (λf.(λx.x)))))) 
+                                   // ((λa.(λf.(λx.((a f) (f x))))) 
+                                   // ((λa.(λf.(λx.((a f) (f x))))) (λf.(λx.x)))))
+type evaluated = Stringify<Eval<parsed>> // (λf.(λxx.(f (f (f (f (f xx)))))))
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # ts-lambda-calc
-Type-level untyped lambda calculus in TypeScript
+Lambda calculus in pure TypeScript type annotations.
+
+## Example
+```ts
+import { Parse, } from "./Parser"
+import { Eval, Stringify, } from "./Semantics"
+
+type ZERO = "(λf.(λx.x))"
+type SUCC = "(λa.(λf.(λx.(a f) (f x))))"
+type ADD = `(λa.(λb.(b ${SUCC} a)))`
+
+type zero = Parse<ZERO>
+type succ = Parse<SUCC>
+type one = Parse<`${SUCC} ${ZERO}`>
+type two = Parse<`${SUCC} (${SUCC} ${ZERO})`>
+
+type rawOne = Stringify<one> // ((λa.(λf.(λx.((a f) (f x))))) (λf.(λx.x)))
+
+type rawTwo = Stringify<two> // ( 
+                             //  (λa.(λf.(λx.((a f) (f x))))) 
+                             //   ((λa.(λf.(λx.((a f) (f x))))) (λf.(λx.x)))
+                             // )
+                             
+type evalZero = Stringify<Eval<zero>>  // (λf.(λx.x))
+type evalOne = Stringify<Eval<one>>  // (λf.(λx.(f x)))
+type evalTwo = Stringify<Eval<two>>  // (λf.(λx.(f (f x))))
+
+// 3 + 2 = 5
+type parsed = Parse<`${ADD} (${SUCC} (${SUCC} (${SUCC} ${ZERO}))) (${SUCC} (${SUCC} ${ZERO}))`>
+type rawParsed = Stringify<parsed> // (((λa.(λb.((b (λa.(λf.(λx.((a f) (f x)))))) a))) 
+                                   // ((λa.(λf.(λx.((a f) (f x))))) 
+                                   // ((λa.(λf.(λx.((a f) (f x)))))
+                                   // ((λa.(λf.(λx.((a f) (f x))))) (λf.(λx.x)))))) 
+                                   // ((λa.(λf.(λx.((a f) (f x))))) 
+                                   // ((λa.(λf.(λx.((a f) (f x))))) (λf.(λx.x)))))
+type evaluated = Stringify<Eval<parsed>> // (λf.(λxx.(f (f (f (f (f xx)))))))
+```
+
+## Limitations
